9c77c0e83e052ebba9de97b1bd1d600acb5f4500
smodugu <smodugu@IST242-NUC011.unomaha.edu>
2019-09-12 14:57:59 -0500

    distance code update

--- src/main/java/com/agilerules/iotled/controller/LEDController.java
+++ src/main/java/com/agilerules/iotled/controller/LEDController.java
@@ -1,11 +1,12 @@
 package com.agilerules.iotled.controller;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinMode;
+import com.pi4j.io.gpio.PinPullResistance;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
 
@@ -13,6 +14,9 @@ import com.pi4j.io.gpio.RaspiPin;
 public class LEDController {
 	
 	public static GpioPinDigitalOutput pin;
+	public static GpioPinDigitalOutput gpioTrigger;
+	public static GpioPinDigitalInput gpioEcho; 
+	
 	
 	@RequestMapping("/")
 	public String greeting(){
@@ -20,13 +24,34 @@ public class LEDController {
 	}
 	
 	@RequestMapping("/light")
-	public String light(){
-		if(pin==null){
+	public String getDistance(){
+		
 			GpioController gpio = GpioFactory.getInstance();
-			pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01,"My LED",PinState.LOW);
-		}
-		pin.toggle();
-		return "Response from light!!";
+			gpioTrigger =  gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00);
+			gpioEcho = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02,PinPullResistance.PULL_DOWN); // Echo pin as INPUT
+			while(true){
+				try {
+				Thread.sleep(2000);
+				gpioTrigger.high(); // Make trigger pin HIGH
+				Thread.sleep((long) 0.01);// Delay for 10 microseconds
+				gpioTrigger.low(); //Make trigger pin LOW
+			
+				while(gpioEcho.isLow()){ //Wait until the ECHO pin gets HIGH
+					
+				}
+				long startTime= System.nanoTime(); // Store the surrent time to calculate ECHO pin HIGH time.
+				while(gpioEcho.isHigh()){ //Wait until the ECHO pin gets LOW
+					
+				}
+				long endTime= System.nanoTime(); // Store the echo pin HIGH end time to calculate ECHO pin HIGH time.
+			
+				System.out.println("Distance :"+((((endTime-startTime)/1e3)/2) / 29.1) +" cm"); //Printing out the distance in cm  
+				Thread.sleep(1000);
+				return "Distance :"+((((endTime-startTime)/1e3)/2) / 29.1) +" cm";
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				}
+			}		
 	}
 
 }
